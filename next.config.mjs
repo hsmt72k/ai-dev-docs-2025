e9539d608f820e4a9b7dd3150118adc4d05c7684
hsmt72k <hsmt72k@gmail.com>
2025-04-10 02:53:34 +0900

    00-00. Introduction

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from 'fumadocs-mdx/next'
 
-const withMDX = createMDX();
+const withMDX = createMDX()
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-};
+  async redirects() {
+    return [
+      {
+        source: '/docs', // リダイレクト元のパス
+        destination: '/docs/hands-on', // リダイレクト先のパス
+        permanent: true, // 永続的なリダイレクト（308ステータスコード）
+      },
+    ]
+  },
+}
 
-export default withMDX(config);
+export default withMDX(config)
