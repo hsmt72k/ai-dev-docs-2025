6d1df2704bac44a3f966038174f12ae9b5ded6cb
hsmt72k <hsmt72k@gmail.com>
2025-04-11 02:29:31 +0900

    revise the overall structure

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,15 @@ const withMDX = createMDX()
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/docs', // リダイレクト元のパス
-        destination: '/docs/hands-on', // リダイレクト先のパス
-        permanent: true, // 永続的なリダイレクト（308ステータスコード）
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/docs', // リダイレクト元のパス
+  //       destination: '/docs/hands-on', // リダイレクト先のパス
+  //       permanent: true, // 永続的なリダイレクト（308ステータスコード）
+  //     },
+  //   ]
+  // },
 }
 
 export default withMDX(config)
