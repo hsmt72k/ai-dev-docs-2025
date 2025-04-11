ce32f49776921f100425552eacf3b8ec4182627c
hsmt72k <hsmt72k@gmail.com>
2025-04-12 02:41:03 +0900

    fix index.mdx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link'
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">
+        ようこそ 生成 AI BoosterCamp へ
+      </h1>
       <p className="text-fd-muted-foreground">
-        AI 開発ハンズオンは&nbsp;
+        生成 AI BoosterCamp は&nbsp;
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
